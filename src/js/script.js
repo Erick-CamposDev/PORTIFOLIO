aadfdde25f12733c93fd2d3c7960276834367c68
Erick Fellipe <efrc22br@gmail.com>
2025-09-29 15:59:25 -0300

    Adicionado script do select

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,6 @@
+const select = document.getElementById("selectSkill");
+const skillCategory = document.querySelectorAll(".skills-category");
+
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
@@ -7,7 +10,32 @@ const observer = new IntersectionObserver((entries) => {
   });
 });
 document
-  .querySelectorAll(
-    ".project-content, .skills-category, .about-content, #contact-me"
-  )
+  .querySelectorAll(".project-content, .about-content, #contact-me")
   .forEach((el) => observer.observe(el));
+
+function selectCategory() {
+  cleanCategory();
+
+  const value = select.value;
+
+  switch (value) {
+    case "Front-end":
+      skillCategory[0].style.display = "flex";
+      break;
+    case "Back-end-DB":
+      skillCategory[1].style.display = "flex";
+      break;
+    case "Frameworks":
+      skillCategory[2].style.display = "flex";
+      break;
+    case "Ferramentas":
+      skillCategory[3].style.display = "flex";
+      break;
+  }
+}
+
+function cleanCategory() {
+  skillCategory.forEach((skill) => (skill.style.display = "none"));
+}
+
+select.addEventListener("change", selectCategory);
