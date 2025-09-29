d77a6722a9e80bd4ac7ee45fa0d5cb9e0f120d8a
Erick Fellipe <efrc22br@gmail.com>
2025-09-29 14:27:47 -0300

    Adições e removidas tecnologias

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,13 @@
-const observer = new IntersectionObserver(entries => {
-  entries.forEach(entry => {
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
     if (entry.isIntersecting) {
-      entry.target.classList.add('show');
+      entry.target.classList.add("show");
       observer.unobserve(entry.target);
     }
   });
 });
-document.querySelectorAll('.project-content, .skills-category').forEach(el => observer.observe(el));
+document
+  .querySelectorAll(
+    ".project-content, .skills-category, .about-content, #contact-me"
+  )
+  .forEach((el) => observer.observe(el));
